5d77df7960b544555b1bd69b4fbdbe5aadb72a09
Lucas Jean Bourgeault <lucas.bourgeault@mobileappscompany.us>
2019-10-22 10:22:50 -0400

    Initial commit

---
+++ app/src/main/java/com/example/w4d1api/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.w4d1api
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
